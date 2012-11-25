7143a45596643daac3a712842b9f32d404c0a207
Brandon <blsmit5728@gmail.com>
2012-11-24 21:53:36 -0500

    added dew_pt and editing file.

--- pi_weather_station/pi_weather.py
+++ pi_weather_station/pi_weather.py
@@ -1,19 +1,48 @@
 import urllib2
 import json
-f = urllib2.urlopen('http://api.wunderground.com/api/f83d46c368361137/geolookup/conditions/q/MD/Columbia.json')
-g = urllib2.urlopen('http://api.wunderground.com/api/f83d46c368361137/geolookup/conditions/q/MD/Gaithersburg.json')
-json_string = f.read()
-json_string_g = g.read()
-parsed_json = json.loads(json_string)
-parsed_json_g = json.loads(json_string_g)
-location = parsed_json['location']['city']
-location_g = parsed_json_g['location']['city']
-temp_f = parsed_json['current_observation']['temp_f']
-temp_f_g = parsed_json_g['current_observation']['temp_f']
-print "Current temperature in %s is: %s" % (location, temp_f)
-print "Current temperature in %s is: %s" % (location_g, temp_f_g)
+import datetime
 
+# Get the current conditions from Columbia
+f = urllib2.urlopen('http://api.wunderground.com/api/f83d46c368361137/geolookup/conditions/q/MD/Columbia.json')
+
+#open the data file to write to.
+f_file = open('./data.dat','w')
+
+# Read the response.
+json_string = f.read()
+
+# Parse the JSON response
+parsed_json = json.loads(json_string)
+
+# Grab the location data.
+location = parsed_json['location']['city']
+
+# grab the temp
+temp_f = parsed_json['current_observation']['temp_f']
+
+# just a temp print
+print "Current temperature in %s is: %s" % (location, temp_f)
+
+#another wind speed
 wind = parsed_json['current_observation']['wind_mph']
 print "Current Wind speed is: %s mph" % (wind)
 
-f.close()
+# dew pt
+dew_pt = parsed_json['current_observation']['dewpoint_f']
+print "Current dew point is: %s mph" % (pew_ptß)
+
+# get the datetime
+now = datetime.datetime.now()
+
+# now just get the date
+str_date = now.month + "-" + now.day + "-" + now.year
+st_date = str(str_date)
+
+# format the output
+st = st_date + " " + temp_f + " " + wind + " " + dew_pt + "\n"
+
+f_file.write(st)
+
+# lets put this in this form: date_temp_wind_dew_
+f_file.close()
+
